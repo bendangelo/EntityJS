44b76db0228ddc004c75b702394898060722a74e
Ben D'Angelo <ben.dangelo2@gmail.com>
2011-12-27 15:51:16 -0500

    wrong file

--- lib/entityjs/command.rb
+++ lib/entityjs/command.rb
@@ -11,6 +11,8 @@ module Entityjs
   class Command
     
     def self.run(command, args=nil)
+      #load config first
+      Entityjs::Config.instance
       
       case command
         when 'new'
