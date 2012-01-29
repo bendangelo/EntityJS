f8d42d49b39cf46b55db874d46ec373b7a36efb8
= <ben.dangelo2@gmail.com>
2012-01-28 21:34:03 -0500

    edited server

--- lib/entityjs/commands/server.rb
+++ lib/entityjs/commands/server.rb
@@ -5,7 +5,18 @@ module Entityjs
   class Server < Sinatra::Base
     
     get '/' do
-      'testing'
+      
+      contents = IO.read("#{Entityjs::root}/public/play.html")
+      
+      contents.gsub("$WIDTH", Config.instance.width)
+      contents.gsub("$HEIGHT", Config.instance.height)
+      contents.gsub("$CANVAS_ID", Config.instance.canvas_id)
+      contents.gsub("$JS", "")
+      
+    end
+    
+    get '/tests' do
+      'Testing stuff soon to come...'
     end
     
   end
