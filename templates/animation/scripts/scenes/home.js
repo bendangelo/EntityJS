5c80e7e14c562a0c2d9dcd67c4c861ced1677f0d
Ben D'Angelo <ben.dangelo2@gmail.com>
2012-05-11 14:04:23 -0400

    animation template

--- templates/animation/scripts/scenes/home.js
+++ templates/animation/scripts/scenes/home.js
@@ -1,20 +1,28 @@
 re.scene('home')
 .enter(function(){
   
-  re.e('sprite flicker hero.png align')
+  re.e('sprite flicker hero.png align keyboard')
   .attr({
     
+    //sets frame size
     sizeX:25,
     sizeY:25,
     
     animate:function(){
-      this.flicker(300, [0, 1, 2, 3, 4]);
+      //first arg is time in milliseconds, array contains the frame numbers to flick through.
+      //time per frame = time / frames.length
+      this.flicker(300, [0, 1, 2, 3, 4, 0]);
     },
     
+    //centers image on screen
     alignHor:0,
-    alignVer:0,
+    alignVer:0
     
   })
+  //listens for keyup event
+  .on('keyup:enter', function(){
+    this.animate();
+  });
   
   //add help text
   re.e('text align')
