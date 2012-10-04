58da3951237d9e65a3c307592b3cc88c367bc1fd
Ben D'Angelo <ben.dangelo2@gmail.com>
2012-10-04 15:17:56 -0400

    fixed hit test bug

--- src/math/hit.js
+++ src/math/hit.js
@@ -14,20 +14,27 @@ re.c('hit')
 	k.hit(entity);
   
 	*/
-	hit:function(x, y, w, h){
+	hit:function(x, y, w, h, rx, ry){
     if(re.is(x, 'object')){
       y = x.posY || x.y;
       w = x.sizeX || x.w;
       h = x.sizeY || x.h;
+      rx = x.regX || 0;
+			ry = x.regY || 0;
       x = x.posX || x.x;
     }
-    
+
+    rx = rx || 0;
+    ry = ry || 0;
+    var regX = this.regX || 0;
+    var regY = this.regY || 0;
+
 		return !
 		(
-		x > this.posX + this.sizeX ||
-		x + w < this.posX ||
-		y > this.posY + this.sizeY ||
-		y + h < this.posY
+		x - rx> this.posX + this.sizeX - regX||
+		x + w - rx < this.posX - regX ||
+		y -ry > this.posY + this.sizeY - regY ||
+		y + h - ry < this.posY - regY
 		);
 	}
 	
