7bd887ae0231edbba964f5fd23b20f6860c5b4ff
Ben D'Angelo <ben.dangelo2@gmail.com>
2012-11-01 17:48:12 -0400

    added group class

---
+++ src/core/group.js
@@ -0,0 +1,15 @@
+/*
+The group class automatically adds / removes entities when added to it.
+
+Usage:
+
+	re.group("monsters");
+
+	re.e('monsters'); //joined group
+
+	re.monsters().first(); // [Object object]
+
+*/
+re.group = re.g = function(title, data){
+
+};
